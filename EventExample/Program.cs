c7f1a6cb5bae3f8688b0916ffe24384ca629981f
Michal Walczynski <mwalczynski@pgs-soft.com>
2017-12-23 20:06:36 +0100

    Add project files.

---
+++ EventExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
